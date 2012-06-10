4feabb295480f880327b14151efef1484bc13310
Adrian Salceanu <adrian@reveloper.com>
2012-06-10 15:42:05 +0200

    cleanup

--- config/valuechecker.php
+++
@@ -1,6 +0,0 @@
-<?
-RESTful_Config::$vc_sports_mappings = array(
-	'non_vc_sports' => array( 'darts', 'dogs', 'gaelic', 'rowing' ), # don't use them -- not available in vc
-	'auto_mapped_sports' => array( 'rugby', 'us sports' ), # use subsport instead of sport
-	'non_mapped_sports' => array( 'nhl' => 'icehockey', 'nfl' => 'americanfootball', 'horse racing' => 'horseracing', 'motor racing' => 'motorracing' ) # use indicated mapping
-);
