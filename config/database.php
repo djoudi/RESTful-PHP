ec92f6f1f7d7b2290787ef966bb4d8c488965e40
Adrian Salceanu <adrian@reveloper.com>
2012-06-10 16:23:32 +0300

    removed login info

--- config/database.php
+++ config/database.php
@@ -2,37 +2,9 @@
 $pdoParams = array( PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8;' );
 
 RESTful_Application::$db_conf['default'] = Zend_Db::factory('Pdo_Mysql', array(
-    'host'     				=> '10.176.80.146',
-    'username' 				=> 'adrian',
-    'password' 				=> 'Schw3pp3s!@#',
-    'dbname'   				=> 'betting_mobile',
+    'host'     				=> 'your_host_here',
+    'username' 				=> 'username',
+    'password' 				=> 'password',
+    'dbname'   				=> 'db_name',
     'driver_options' 		=> $pdoParams,
 ));
-
-/*
-RESTful_Application::$db_conf['default'] = Zend_Db::factory('Pdo_Mysql', array(
-    'host'     				=> 'localhost',
-    'username' 				=> 'root',
-    'password' 				=> 'root',
-    'dbname'   				=> 'betting_mobile',
-    'driver_options' 		=> $pdoParams,
-));
-*/
-
-RESTful_Application::$db_conf['valuechecker'] = Zend_Db::factory('Pdo_Mysql', array(
-    'host'     				=> '192.168.100.208',
-    'username' 				=> 'vc_masterdb_user',
-    'password' 				=> 'wdG201kh49DuuSe',
-    'dbname'   				=> 'odds101',
-    'driver_options' 		=> $pdoParams,
-));
-
-/*
-RESTful_Application::$db_conf['valuechecker'] = Zend_Db::factory('Pdo_Mysql', array(
-    'host'     				=> '94.236.99.181',
-    'username' 				=> 'adrian',
-    'password' 				=> 'Schw3pp3s!@#',
-    'dbname'   				=> 'odds101',
-    'driver_options' 		=> $pdoParams,
-));
-*/
